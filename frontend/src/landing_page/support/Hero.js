0b66ec64ecc4d67640a1ef7e7d31e04b5388d4f4
hemant16singh <hemantkumar90916@gmail.com>
2026-05-04 01:43:11 +0530

    fixed frontend

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,34 +1,94 @@
-import React from 'react';
+// import React from 'react';
 
+
+// function Hero() {
+//   return (
+//     <section className="container-fluid" id="supportHero">
+//       <div className="p-5 " id="supportWrapper">
+//         <h4>Support Portal</h4>
+//         <a href="">Track Tickets</a>
+//       </div>
+//       <div className="row p-5 m-3">
+//         <div className="col-6 p-3 ">
+//           <h1 className="fs-3">
+//             Search for an answer or browse help topics to create a ticket
+//           </h1>
+//           <input placeholder="Eg. how do I activate F&O" />
+//           <br />
+//           <a href="" class="me-3" >Track account opening</a>
+//           <a href=""class="me-3">Track segment activation</a>
+//           <a href=""class="me-3">Intraday margins</a>
+//           <a href="">Kite user manual</a>
+//         </div>
+//         <div className="col-6 p-3">
+//           <h1 className="fs-3">Featured</h1>
+//           <ol>
+//             <li>
+//               <a href="">Current Takeovers and Delisting - January 2024</a>
+              
+//             </li>
+//             <li>
+//               <a href="">Latest Intraday leverages - MIS & CO</a>
+//             </li>
+//           </ol>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Hero;
+
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
     <section className="container-fluid" id="supportHero">
-      <div className="p-5 " id="supportWrapper">
+      <div className="p-5" id="supportWrapper">
         <h4>Support Portal</h4>
-        <a href="">Track Tickets</a>
+        <Link to="/track-tickets">Track Tickets</Link>
       </div>
+
       <div className="row p-5 m-3">
-        <div className="col-6 p-3 ">
+        <div className="col-6 p-3">
           <h1 className="fs-3">
             Search for an answer or browse help topics to create a ticket
           </h1>
+
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-          <a href="" class="me-3" >Track account opening</a>
-          <a href=""class="me-3">Track segment activation</a>
-          <a href=""class="me-3">Intraday margins</a>
-          <a href="">Kite user manual</a>
+
+          <Link to="/track-account" className="me-3">
+            Track account opening
+          </Link>
+
+          <Link to="/segment-activation" className="me-3">
+            Track segment activation
+          </Link>
+
+          <Link to="/intraday-margins" className="me-3">
+            Intraday margins
+          </Link>
+
+          <Link to="/kite-manual">
+            Kite user manual
+          </Link>
         </div>
+
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
-              <a href="">Current Takeovers and Delisting - January 2024</a>
-              
+              <Link to="/takeovers">
+                Current Takeovers and Delisting - January 2024
+              </Link>
             </li>
+
             <li>
-              <a href="">Latest Intraday leverages - MIS & CO</a>
+              <Link to="/intraday-leverage">
+                Latest Intraday leverages - MIS & CO
+              </Link>
             </li>
           </ol>
         </div>
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,10 +1,23 @@
+// import React from "react";
+
+// import Hero from "./Hero";
+// import CreateTicket from "./CreateTicket";
+
+// import Navbar from "../Navbar";
+// import Footer from "../Footer";
+
+// function SupportPage() {
+//   return (
+//     <>
+//       <Hero />
+//       <CreateTicket />
+//     </>
+//   );
+// }
 import React from "react";
 
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
-
-import Navbar from "../Navbar";
-import Footer from "../Footer";
 
 function SupportPage() {
   return (
