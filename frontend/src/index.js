6ae9ac81046c459b5fb9d864c0bb4182e539198e
hemant16singh <hemantkumar90916@gmail.com>
2026-05-08 20:17:34 +0530

    changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,34 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import './index.css';
-import HomePage from './landing_page/home/HomePage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./index.css";
+
+/* Bootstrap */
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+/* Icons */
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+/* Pages */
+import HomePage from "./landing_page/home/HomePage";
 import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
 import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
 import SupportPage from "./landing_page/support/SupportPage";
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Navbar from './landing_page/Navbar';
-import Footer from './landing_page/Footer';
-import NotFound from './landing_page/NotFound';
+/* Components */
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
+import NotFound from "./landing_page/NotFound";
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Navbar/>
+    <Navbar />
+
     <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/about' element={<AboutPage/>}/>
-      <Route path='/product' element={<ProductPage/>}/>
-      <Route path='/pricing' element={<PricingPage/>}/>
-      <Route path='/support' element={<SupportPage/>}/>
-      <Route path='/*' element={<NotFound/>}/>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/product" element={<ProductPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
-    <Footer/>
+
+    <Footer />
   </BrowserRouter>
 );
--- frontend/src/landing_page/home/Award.js
+++ frontend/src/landing_page/home/Award.js
@@ -3,56 +3,75 @@ import React from "react";
 function Awards() {
   return (
     <div className="container mt-5">
+
       <div className="row align-items-center">
 
         {/* LEFT IMAGE */}
-        <div className="col-lg-6 col-md-12 text-center p-3">
+        <div className="col-lg-6 col-12 text-center p-3">
+
           <img
             src="/media/images/largestBroker.svg"
             alt="Largest Broker"
             className="img-fluid"
             style={{ maxWidth: "100%" }}
           />
+
         </div>
 
         {/* RIGHT CONTENT */}
-        <div className="col-lg-6 col-md-12 p-lg-5 p-3">
-          <h1 className="mb-4">Largest stock broker in India</h1>
+        <div className="col-lg-6 col-12 p-lg-5 p-3">
+
+          <h1 className="mb-4">
+            Largest stock broker in India
+          </h1>
 
           <p className="mb-5 text-muted">
-            2+ million Zerodha clients contribute to over 15% of all volumes
-            in India daily by trading and investing in:
+            2+ million Zerodha clients contribute to over
+            15% of all volumes in India daily by trading
+            and investing in:
           </p>
 
+          {/* LISTS */}
           <div className="row">
+
             <div className="col-6">
+
               <ul>
                 <li><p>Futures and Options</p></li>
                 <li><p>Commodity Derivatives</p></li>
                 <li><p>Currency Derivatives</p></li>
               </ul>
+
             </div>
 
             <div className="col-6">
+
               <ul>
                 <li><p>Equity Delivery</p></li>
                 <li><p>Equity Intraday</p></li>
                 <li><p>Mutual Funds</p></li>
               </ul>
+
             </div>
+
           </div>
 
+          {/* PRESS LOGOS */}
           <div className="text-center text-lg-start mt-4">
+
             <img
               src="/media/images/pressLogos.png"
               alt="Press Logos"
               className="img-fluid"
               style={{ maxWidth: "90%" }}
             />
+
           </div>
+
         </div>
 
       </div>
+
     </div>
   );
 }
